556735ee80b8a2b48af55d5538dd2ea703622dda
Helcio <helcioanicio19@gmail.com>
2024-10-03 22:23:13 -0300

    Feat: Added Menu and Sheet components, and set header to sheet

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,37 +1,73 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 import { ListFilter, CirclePlus } from 'lucide-react';
 
 export const Header = () => {
   return (
-    <header className='flex justify-between items-end p-2 w-full'>
-      <div className='w-1/3 '>
-        <Image
-          src='/logo'
-          alt='Logo - Imagem de um touro e uma ovelha'
-          width={100}
-          height={100}
-          className='size-24'
-        />
-      </div>
-      <div className='flex w-full flex-col gap-4 items-end '>
-        <div className='flex gap-3'>
-          <Button className='flex gap-2 rounded-2xl p-1 '>
-            Filtros <ListFilter />
-          </Button>
-          <Button className='flex gap-2 rounded-2xl p-1 '>
-            Adicionar <CirclePlus />
-          </Button>
+    <Sheet>
+      <header className='flex justify-between items-end p-2 w-full'>
+        <div className='w-1/3 '>
+          <Image
+            src='/logo'
+            alt='Logo - Imagem de um touro e uma ovelha'
+            width={100}
+            height={100}
+            className='size-24'
+          />
         </div>
-        <input
-          className='w-full max-w-60 bg-transparent border rounded-xl shadow-md p-1'
-          type='search'
-          name='inputSearch'
-          id='inputSearch'
-          placeholder='Pesquisar ID'
-        />
-      </div>
-    </header>
+        <div className='flex w-full flex-col gap-4 items-end '>
+          <div className='flex gap-3'>
+            <Button className='flex gap-2 rounded-2xl p-1 '>
+              Filtros <ListFilter />
+            </Button>
+            <SheetTrigger asChild>
+              <Button className='flex gap-2 rounded-2xl p-1 '>
+                Adicionar <CirclePlus />
+              </Button>
+            </SheetTrigger>
+          </div>
+          <input
+            className='w-full max-w-60 outline-none bg-transparent border rounded-xl shadow-md p-1'
+            type='search'
+            name='inputSearch'
+            id='inputSearch'
+            placeholder='Pesquisar ID'
+          />
+        </div>
+      </header>
+
+      <SheetContent side='bottom'>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when youre done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className='grid gap-4 py-4'>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <p className='text-right'>Name</p>
+          </div>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <p className='text-right'>Username</p>
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type='submit'>Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
---
+++ src/components/ui/menu.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import { GiCow } from 'react-icons/gi';
+import { IoMaleFemaleSharp } from 'react-icons/io5';
+import { FaRegMoneyBillAlt } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
+
+export const MenuNavegation = () => {
+  return (
+    <footer className='absolute bottom-0 -z-10 bg-primary w-full h-24 rounded-t-3xl'>
+      <section className='relative z-20 text-primary-foreground flex justify-between px-5 mt-2'>
+        <Link href='/'>
+          <div className='flex flex-col gap-1 items-center bg-primary text-black rounded-full size-16 relative -mt-4 scale-125'>
+            <GiCow size={35} />
+            <p>Animal</p>
+          </div>
+        </Link>
+        <Link href='/'>
+          <div className='flex flex-col gap-1 items-center rounded-full size-16 relative'>
+            <IoMaleFemaleSharp size={35} />
+            <p>Reprodução</p>
+          </div>
+        </Link>
+        <Link href='/'>
+          <div className='flex flex-col gap-1 items-center rounded-full size-16 relative'>
+            <FaRegMoneyBillAlt size={35} />
+            <p>Financeiro</p>
+          </div>
+        </Link>
+        <Link href='/'>
+          <div className='flex flex-col gap-1 items-center rounded-full size-16 relative'>
+            <CgProfile size={35} />
+            <p>Perfil</p>
+          </div>
+        </Link>
+      </section>
+    </footer>
+  );
+};
