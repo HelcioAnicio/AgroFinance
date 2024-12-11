bef8daef3b4bb9cec62f2a493ea4c50f74efb4e4
Helcio <helcioanicio19@gmail.com>
2024-12-11 16:36:00 -0300

    Feat: Used Skeleton component during loading status

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Header = () => {
   const { status, data } = useSession();
@@ -26,39 +27,45 @@ export const Header = () => {
       </div>
 
       <nav className="flex w-full flex-col items-end gap-4">
-        <div>
-          {status === "authenticated" && (
-            <div className="flex items-center gap-1">
-              {data?.user?.name}
-              <Avatar className="size-8">
-                <AvatarImage
-                  src={data?.user?.image ?? undefined}
-                  alt="Image from google profile"
-                />
-                <AvatarFallback className="text-foreground">
-                  {data?.user?.name?.charAt(0)}{" "}
-                </AvatarFallback>
-              </Avatar>
-              <Button
-                className="scale-75 bg-secondary p-1 text-accent"
-                onClick={handleLogoutClick}
-              >
-                Logout
-              </Button>
-            </div>
-          )}
+        {status === "authenticated" && (
+          <div className="flex items-center gap-1">
+            {data?.user?.name}
+            <Avatar className="size-8">
+              <AvatarImage
+                src={data?.user?.image ?? undefined}
+                alt="Image from google profile"
+              />
+              <AvatarFallback className="text-foreground">
+                {data?.user?.name?.charAt(0)}{" "}
+              </AvatarFallback>
+            </Avatar>
+            <Button
+              className="scale-75 bg-secondary p-1 text-accent"
+              onClick={handleLogoutClick}
+            >
+              Logout
+            </Button>
+          </div>
+        )}
 
-          {status === "unauthenticated" && (
-            <div className="m-auto flex w-full max-w-screen-lg justify-between py-4">
-              <h1>
-                Olá, se deseja logar e acessar o nosso dashboard{" "}
-                <Button>
-                  <Link href="/login">click aqui</Link>{" "}
-                </Button>
-              </h1>
-            </div>
-          )}
-        </div>
+        {status === "loading" && (
+          <div className="flex items-center gap-1">
+            <Skeleton className="h-4 w-16" />
+            <Skeleton className="size-8 rounded-full" />
+            <Skeleton className="w-10 h-7" />
+          </div>
+        )}
+
+        {status === "unauthenticated" && (
+          <div className="m-auto flex w-full max-w-screen-lg justify-between py-4">
+            <h1>
+              Olá, se deseja logar e acessar o nosso dashboard{" "}
+              <Button>
+                <Link href="/login">click aqui</Link>{" "}
+              </Button>
+            </h1>
+          </div>
+        )}
       </nav>
     </header>
   );
