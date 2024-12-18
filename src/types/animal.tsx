0f5d60fe5e4aed5f9c7a4f4482078ffeeb92b76e
Helcio <helcioanicio19@gmail.com>
2024-12-18 10:06:16 -0300

    Feat: Improve interface.

--- src/types/animal.tsx
+++ src/types/animal.tsx
@@ -1,4 +1,7 @@
+import { User } from "./user";
+
 export interface Animal {
+  status: string;
   id: string;
   manualId: number;
   gender: string;
@@ -13,9 +16,22 @@ export interface Animal {
   bullId: string | null;
   protocol: string | null;
   andrological: string | null;
-  fetalGender: string | null;
   expectedDueDate: Date | null;
+  fetalGender: string | null;
   bullIatf: string | null;
   bodyConditionScore: number | null;
+  observations: string | null;
+  createdAt: Date;
+  updatedAt: Date;
   ownerId: string;
+  bull?: Animal;
+  offspringFromBull?: Animal[];
+  father?: Animal;
+  offspringFromFather?: Animal[];
+  mother?: Animal;
+  offspringFromMother?: Animal[];
+  owner?: User;
+  // dewormings?: Deworming[];
+  // diseases?: Disease[];
+  // vaccines?: Vaccine[];
 }
