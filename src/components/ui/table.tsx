d258d6b5763c53afe3342c29d059b0ed6367ce66
Helcio Anicio <helcioanicio19@gmail.com>
2024-12-31 06:55:37 -0300

    Feat: Added link to parents, improve logic and added status field

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SquareArrowOutUpLeft } from "lucide-react";
+import { Circle, SquareArrowOutUpLeft } from "lucide-react";
 import Link from "next/link";
 import { Animal } from "@/types/animal";
 import { User } from "@/types/user";
@@ -38,7 +38,7 @@ export const Table: React.FC<TableProps> = ({ animals, users }) => {
     if (inputValue === undefined) {
       setListAnimals(originalAnimals);
     } else {
-      const filtered = listAnimals
+      const filtered = originalAnimals
         .filter((animal) => animal.manualId == inputValue)
         .sort((a, b) => (a.manualId ?? 0) - (b.manualId ?? 0));
       setListAnimals(filtered);
@@ -62,12 +62,12 @@ export const Table: React.FC<TableProps> = ({ animals, users }) => {
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(
-      event.target.value ? parseInt(event.target.value) : undefined,
+      event.target.value ? parseFloat(event.target.value) : undefined,
     );
   };
 
   return (
-    <main className="overflow-x-auto scroll-smooth pb-5">
+    <main className="overflow-x-auto scroll-smooth pb-5 max-w-[750px] m-auto">
       <div className="sticky right-0 top-0 z-50 w-full">
         <div className="flex w-full justify-between gap-10 px-1 sm:justify-end">
           <input
@@ -109,9 +109,10 @@ export const Table: React.FC<TableProps> = ({ animals, users }) => {
         </div>
       </div>
       <br />
-      <table className="m-auto min-w-[700px] border-collapse text-left">
+      <table className="m-auto min-w-[750px] border-collapse text-left xl:text-sm">
         <thead className="border-collapse bg-primary text-background">
           <tr>
+            <th className="px-1 py-2">Status</th>
             <th className="px-1 py-2">ID</th>
             <th className="px-1 py-2">Raça</th>
             <th className="px-1 py-2">Sexo</th>
@@ -125,18 +126,30 @@ export const Table: React.FC<TableProps> = ({ animals, users }) => {
         </thead>
         <tbody className="h-[calc(100%-200px)] overflow-y-auto scroll-smooth">
           {listAnimals.map((animal: Animal, index: number) => {
-            const mother: Animal | undefined = animals.find(
-              (a) => a.id === animal.motherId,
-            );
-            const father: Animal | undefined = animals.find(
-              (a) => a.id === animal.fatherId,
-            );
 
             return (
               <tr
                 key={animal.id}
                 className={`${index % 2 === 0 ? "bg-muted" : ""}`}
               >
+                <td className="px-1 py-3">
+                  <Link
+                    href={`dashboard/${animal.id}`}
+                    className="block h-full w-full"
+                  >
+                    {animal?.status === "active" ? (
+                      <>
+                        <Circle className="inline-block size-3 rounded-full bg-green-400 text-green-400" />
+                        {" "}Ativo
+                      </>
+                    ) : (
+                      <>
+                        <Circle className="text-graybg-gray-500 inline-block size-3 rounded-full bg-gray-500" />
+                        {" "}Inativo
+                      </>
+                    )}
+                  </Link>
+                </td>
                 <td className="px-1 py-3">
                   <Link
                     href={`dashboard/${animal.id}`}
@@ -163,22 +176,18 @@ export const Table: React.FC<TableProps> = ({ animals, users }) => {
                 </td>
                 <td className="px-1 py-3">
                   <Link
-                    href={`dashboard/${animal.id}`}
+                    href={`dashboard/${animal.motherId}`}
                     className="block h-full w-full"
                   >
-                    {animal.motherId === null
-                      ? "Comercial"
-                      : mother
-                        ? `Vaca ${mother.manualId}`
-                        : "Comercial"}
+                    {animal.motherId ? `Vaca ${animal.mother?.id}` : "Comercial"}
                   </Link>
                 </td>
                 <td className="px-1 py-3">
                   <Link
-                    href={`dashboard/${animal.id}`}
+                    href={`dashboard/${animal.fatherId}`}
                     className="block h-full w-full"
                   >
-                    {father ? `Vaca ${father.manualId}` : "Comercial"}
+                    {animal.fatherId ? `Touro ${animal.father?.manualId}` : "Comercial"}
                   </Link>
                 </td>
                 <td className="px-1 py-3">
