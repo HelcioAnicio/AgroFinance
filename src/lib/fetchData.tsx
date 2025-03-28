ecb7d993626ddadcc89c8b246488bf52bc361a8a
Helcio Anicio <helcioanicio19@gmail.com>
2025-03-28 06:43:06 -0300

    Feat: Request vaccine table on supabase

--- src/lib/fetchData.tsx
+++ src/lib/fetchData.tsx
@@ -2,6 +2,7 @@
 import prisma from '@/lib/useDataBase';
 import { Animal } from '@/types/animal';
 import { User } from '@/types/user';
+import { Vaccine } from '@/types/vaccine';
 
 export const fetchAnimals = async (ownerId?: string): Promise<Animal[]> => {
   try {
@@ -14,11 +15,11 @@ export const fetchAnimals = async (ownerId?: string): Promise<Animal[]> => {
       const bIsNumber = !isNaN(Number(b.manualId));
 
       if (!aIsNumber && !bIsNumber) {
-        return String(a.manualId).localeCompare(String(b.manualId)); // Comparação alfabética
+        return String(a.manualId).localeCompare(String(b.manualId));
       } else if (aIsNumber && bIsNumber) {
-        return Number(a.manualId) - Number(b.manualId); // Comparação numérica
+        return Number(a.manualId) - Number(b.manualId);
       } else {
-        return aIsNumber ? 1 : -1; // Strings antes de números
+        return aIsNumber ? 1 : -1;
       }
     });
 
@@ -31,4 +32,17 @@ export const fetchAnimals = async (ownerId?: string): Promise<Animal[]> => {
 
 export const fetchUsers = async (): Promise<User[]> => {
   return await prisma.user.findMany();
+};
+
+export const fetchVaccines = async (): Promise<Vaccine[]> => {
+  const vaccines = await prisma.vaccine.findMany();
+  const vaccinesWithAnimal = await Promise.all(
+    vaccines.map(async (vaccine) => ({
+      ...vaccine,
+      animalId: await prisma.animal.findUnique({
+        where: { id: vaccine.animalId },
+      }),
+    }))
+  );
+  return vaccinesWithAnimal as Vaccine[];
 };
