f609228d23000d91e4f47b69899b0ddcc0f3e15f
Helcio Anicio <helcioanicio19@gmail.com>
2025-06-18 05:43:48 -0300

    Fix: Fixed request to show only owner animal

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,10 +2,15 @@ import { Header } from '@/components/ui/header';
 import { MenuNavegation } from '@/components/ui/menu';
 import { Table } from '@/components/ui/table';
 import { fetchAnimals, fetchUsers } from '@/lib/fetchData';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
 const Dashboard = async () => {
-  const animals = await fetchAnimals();
+  const session = await getServerSession(authOptions);
   const users = await fetchUsers();
+  const userEmail = users.find((user) => user.email === session?.user?.email);
+
+  const animals = await fetchAnimals(userEmail?.id ?? undefined);
 
   return (
     <div className="h-full overflow-hidden text-xs">
