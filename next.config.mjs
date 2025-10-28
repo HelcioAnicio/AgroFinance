67a460a0a3937a5828765794aba4d691ee427738
Helcio <helcioanicio19@gmail.com>
2025-10-28 15:22:15 +0000

    Chore: Added server actions and bodySize

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'cautious-space-goldfish-j6jrrqg9gjr3ppxg-3000.app.github.dev/',
+        'localhost:3000',
+        'agro-finance-real.vercel.app/',
+        'agro-finance-real-git-developer-helcioanicios-projects.vercel.app/',
+      ],
+      bodySizeLimit: '10mb',
+    },
+  },
+};
 
 export default nextConfig;
