c0d315ef336703514d999beb6fca3d8091b532a6
Helcio <helcioanicio19@gmail.com>
2025-10-28 20:25:27 +0000

    Refactor: Implement creation notification and changed to axios request

--- src/app/api/addAnimals/route.ts
+++ src/app/api/addAnimals/route.ts
@@ -3,6 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { fetchUsers } from '@/lib/fetchData';
 import prisma from '@/lib/useDataBase';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
@@ -15,15 +16,9 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: 'User not found' }, { status: 404 });
 
   try {
-    const text = await req.text();
-    console.log('raw request body:', text);
+    const json = await req.json();
 
-    const body = text ? JSON.parse(text) : {};
-    console.log('parsed body:', body);
-
-    // const json = await req.json();
-
-    const { allDataForm } = body || {};
+    const { allDataForm } = json;
     if (!allDataForm) {
       return NextResponse.json(
         { message: 'Dados do formulário não enviados' },
@@ -33,9 +28,34 @@ export async function POST(req: NextRequest) {
 
     const createAnimal = await prisma.animal.create({ data: allDataForm });
 
+    const expectedDueDate = new Date(allDataForm.expectedDueDate);
+    const notifyAt = expectedDueDate.setMonth(expectedDueDate.getMonth() - 1);
+
+    let createNotification;
+
+    if (
+      allDataForm.reproductiveStatus === 'pregnant' &&
+      allDataForm.expectedDueDate !== null
+    ) {
+      if (expectedDueDate.getFullYear() >= new Date().getFullYear()) {
+        if (expectedDueDate.getMonth() >= new Date().getMonth())
+          createNotification = await prisma.notification.create({
+            data: {
+              id: uuidv4(),
+              message: `Seu animal ${allDataForm.manualId} está próximo ao parto.`,
+              notifyAt: new Date(notifyAt!),
+              read: false,
+              userId: allDataForm.ownerId,
+              animalId: allDataForm.id,
+            },
+          });
+      }
+    }
+
     return NextResponse.json({
       message: 'Animal cadastrado com sucesso',
       createAnimal,
+      createNotification,
     });
   } catch (error: unknown) {
     console.error('Erro ao cadastrar animal:', error);
