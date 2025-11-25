210fdb0bed71359946b100f10e130da1b563b927
Helcio <helcioanicio19@gmail.com>
2025-11-25 10:04:59 -0300

    Fix: Refactor imports in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
-import { defineConfig, env } from '@prisma/config';
+import type { PrismaConfig } from "prisma";
+import { env } from "prisma/config";
+
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -11,3 +13,4 @@ export default defineConfig({
     directUrl: env('DIRECT_URL'),
   },
 });
+
