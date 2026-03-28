c44c32313be7544ac121115fba3852c95b310799
Helcio <helcioanicio19@gmail.com>
2026-03-28 19:27:16 -0300

    Atualizar o header.tsx

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -61,7 +61,7 @@ export const Header: React.FC<TableProps> = ({ notifications }) => {
           <li>
             <Link
               href={'/dashboard/reproduction/'}
-              className="relative flex items-center gap-1 transition-all duration-300 after:absolute after:-bottom-1 after:left-0 after:h-px after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:text-secondary hover:after:w-full"
+              className="relative hidden lg:flex items-center gap-1 transition-all duration-300 after:absolute after:-bottom-1 after:left-0 after:h-px after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:text-secondary hover:after:w-full"
             >
               <IoMaleFemaleSharp size={20} />
               Reprodução
@@ -70,7 +70,7 @@ export const Header: React.FC<TableProps> = ({ notifications }) => {
           <li>
             <Link
               href={'/'}
-              className="after:duration-300after:left-0 relative flex items-center gap-1 transition-all duration-300 after:absolute after:-bottom-1 after:h-px after:w-0 after:bg-foreground after:transition-all hover:text-secondary hover:after:w-full"
+              className="after:duration-300after:left-0 relative hidden lg:flex items-center gap-1 transition-all duration-300 after:absolute after:-bottom-1 after:h-px after:w-0 after:bg-foreground after:transition-all hover:text-secondary hover:after:w-full"
             >
               <FaRegMoneyBillAlt size={20} /> Financeiro
             </Link>
@@ -78,7 +78,7 @@ export const Header: React.FC<TableProps> = ({ notifications }) => {
           <li>
             <Link
               href={'/dashboard/profile'}
-              className="relative flex items-center gap-1 transition-all duration-300 after:absolute after:-bottom-1 after:left-0 after:h-px after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:text-secondary hover:after:w-full"
+              className="relative hidden lg:flex items-center gap-1 transition-all duration-300 after:absolute after:-bottom-1 after:left-0 after:h-px after:w-0 after:bg-foreground after:transition-all after:duration-300 hover:text-secondary hover:after:w-full"
             >
               <CgProfile size={20} />
               Perfil
